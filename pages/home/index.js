f3ffec2bbb797fb62edc03b21933fb7f577f2246
Cahya <cahyasaga321@gmail.com>
2022-06-30 11:54:35 +0800

    add register but stil's bug

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
-const home = () => {
-  return <div>home</div>;
+const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    let token = sessionStorage.getItem("Token");
+    if (!token) return router.push("/register");
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,9 +1,92 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { app } from "firebaseConfig";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { async } from "@firebase/util";
+import { useRouter } from "next/router";
 const Register = () => {
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const signUp = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // const log = await response.user;
+      // console.log(log);
+      sessionStorage.setItem("Token", await response.user.accessToken);
+      await router.push("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const signUpWithGoogle = async () => {
+    try {
+      const response = await signInWithPopup(auth, googleProvider);
+      // const log = await response.user;
+      // console.log(log);
+      sessionStorage.setItem("Token", await response.user.accessToken);
+
+      console.log(await response.user);
+      await router.push("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const signUpWithGithub = async () => {
+    try {
+      const response = await signInWithPopup(auth, githubProvider);
+      // const log = await response.user;
+      // console.log(log);
+      sessionStorage.setItem("Token", await response.user.accessToken);
+
+      console.log(await response.user);
+      await router.push("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    let token = sessionStorage.getItem("Token");
+    if (token) return router.push("/home");
+  }, []);
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="flex flex-col p-5 gap-4">
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button className="flex justify-center" onClick={signUp}>
+        Sign Up
+      </button>
+      <button className="flex justify-center" onClick={signUpWithGoogle}>
+        Sign Up with Google
+      </button>
+      <button className="flex justify-center" onClick={signUpWithGithub}>
+        Sign Up with Github
+      </button>
     </div>
   );
 };
